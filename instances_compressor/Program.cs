7efb0cf70ffe93a08f50e82308a17313f6478034
rae-destil <lawful.interference@proton.me>
2025-07-19 16:14:34 +0200

    Rename BSDriverPlugin folder. Add dummy program to compress the json

---
+++ instances_compressor/Program.cs
@@ -0,0 +1,11 @@
+using System.IO;
+using System.IO.Compression;
+
+var solutionRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\..")); // 4 levels up
+var input = Path.Combine(solutionRoot, "BSDriverPlugin", "Data", "instances_data.json");
+var output = Path.Combine(solutionRoot, "BSDriverPlugin", "Data", "instances_data.json.gz");
+
+using var inFile = File.OpenRead(input);
+using var outFile = File.Create(output);
+using var compressor = new GZipStream(outFile, CompressionLevel.Optimal);
+inFile.CopyTo(compressor);
